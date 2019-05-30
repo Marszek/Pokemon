f4b01aa4523e10eb393701d1933ca491d09648fa
Marcin Wdowiak <mardowiak@gmail.com>
2019-05-31 00:23:50 +0200

    Klasa zawierająca metodę main

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ObslugaWyboru ow= new ObslugaWyboru();
+        System.out.println("Witaj przyszły trenerze Pokemon. Czy chcesz wybrac swojego pierwszego pokemona czy chcesz go wylosowac?\nWybierz opcję: \nLosuj \nWybierz \nWyjście");
+        ow.wybierzPokemona();
+    }
+}

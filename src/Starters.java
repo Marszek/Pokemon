ac8cdfa4f6976d5b16ea01b591c20c9b3db3dce7
Marcin Wdowiak <mardowiak@gmail.com>
2019-05-31 00:18:04 +0200

    Klasa przechowująca 3 obiekty klasy Pokemon

---
+++ src/Starters.java
@@ -0,0 +1,9 @@
+//Klasa przechowująca 3 pokemony startowe
+
+public class Starters {
+
+    Pokemon bulbasaur= new Pokemon("Bulbasaur","trawa");
+    Pokemon charmander= new Pokemon("Charmander","ogień");
+    Pokemon squirtle= new Pokemon("Squirtle","woda");
+
+}
